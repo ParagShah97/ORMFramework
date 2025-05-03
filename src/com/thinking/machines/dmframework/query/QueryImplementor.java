ee34e2ac60bc17d05494efdd3c063591625f3be6
ParagShah97 <parag.shah97@gmail.com>
2025-05-03 11:53:15 -0400

    Complete ORM framework

---
+++ src/com/thinking/machines/dmframework/query/QueryImplementor.java
@@ -0,0 +1,10 @@
+package com.thinking.machines.dmframework.query;
+
+import com.thinking.machines.dmframework.exceptions.*;
+import java.util.*;
+
+public interface QueryImplementor<T> {
+    public List<T> query() throws DMFrameworkException;
+
+    public void addExpression(Expression<T> expression);
+}
